6f36848ac21e5e77d893c0ef2e692395234b4089
Chang Li <changli@Changs-MacBook-Pro-2.local>
2024-11-04 20:08:34 -0800

    adding new message here

---
+++ README.md
@@ -0,0 +1,10 @@
+# Introduction to Git
+
+## Create a new Git Repo
+
+```
+git init
+```
+## Check the status of your repo
+```
+## To add 
---
+++ hello.py
@@ -0,0 +1,14 @@
+print("hello world")
+
+> ls - list directory
+> cd - change directory
+> pwd - print working directory
+
+For Mac
+-------
+xcode-selected --install
+""""
+
+print("Hello, world!!!!")
+
+"git bash" 
